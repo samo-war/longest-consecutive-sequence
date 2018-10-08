50a30df31a505241fa8e4529ccff3085a2f759d6
samo-war <samo_war@mail.ru>
2018-10-08 03:50:29 +0300

    Task 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  const arr = array;
+  let long = 1;
+  let maxLong = 1;
+  if (arr.length == 0) {
+    return 0;
+  } else if (arr.length == 1) {
+    return 1;
+  } else {
+    for (let i = 1; i < arr.length; i++) {
+      let curNum = arr[i];
+      let prevNum = arr[i-1];
+      if (curNum == (prevNum + 1)) {
+        long += 1;
+          if (long > maxLong) {
+            maxLong = long;
+          }
+      } else {
+        long = 1;
+      }
+    }
+  }
+  return maxLong;
+}  
